0df724a400476a6859b77f870e7a7a4acafa999d
NicoOhR <nohayonr@gmail.com>
2024-11-30 16:02:59 -0600

    working id, will implement CAS later :)

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"strconv"
 	"sync"
 
 	ms "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-var COUNT int = -1
+var COUNT int = 0
 var mu = sync.Mutex{}
 
-func unique_id() int {
+func unique_id(node *ms.Node) int {
 	COUNT++
-	return COUNT
+	// count = 0
+	id_string := node.ID()
+	node_number, _ := strconv.Atoi(id_string[1:])
+	len := len(node.NodeIDs())
+	return COUNT*len + node_number
 }
 
 func main() {
@@ -26,7 +31,7 @@ func main() {
 
 		body["type"] = "generate_ok"
 		mu.Lock()
-		body["id"] = unique_id() * len(id_node.NodeIDs())
+		body["id"] = unique_id(id_node)
 		mu.Unlock()
 		id_node.Reply(msg, body)
 		return nil
